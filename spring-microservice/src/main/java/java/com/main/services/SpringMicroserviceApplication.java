6f777d91739c0189e454274ae5c9a25a323d8242
Khanchai <kanchaikun@gmail.com>
2021-07-04 19:02:02 +0700

    first commit

---
+++ spring-microservice/src/main/java/java/com/main/services/SpringMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package java.com.main.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroserviceApplication.class, args);
+	}
+
+}
